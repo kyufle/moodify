52ed4c4b96043382b9a3a5eba4e59b82b8b03b8c
ruthromerocarretero <rromerocarretero.cf@iesesteveterradas.cat>
2026-03-29 16:43:35 +0200

    laravel migrations (create database and seeders)

--- moodify_backend/database/seeders/DatabaseSeeder.php
+++ moodify_backend/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,8 +19,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Valeria',
+            'email' => 'valeria@gmail.com',
+            'password' => Hash::make('test'),
+            'type_user' => 'admin',
+            'username' => 'valeria',
         ]);
     }
 }
